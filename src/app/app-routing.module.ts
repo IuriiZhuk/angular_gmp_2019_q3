44e2957daadb9899b4c4124f8b226417abb416ed
Iurii Zhuk <iurii_zhuk@epam.com>
2019-10-14 11:46:15 +0300

    agm-t1 add base styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoursesComponent } from './courses/pages/courses/courses.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CoursesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
