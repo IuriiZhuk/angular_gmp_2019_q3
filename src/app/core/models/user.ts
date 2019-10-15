560a757c32d4964c2efe309b1e177581352ef0a2
Iurii Zhuk <iurii_zhuk@epam.com>
2019-10-15 15:11:36 +0300

    agm-t1 add class Course, User

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,5 +1,20 @@
-export interface User {
+export interface IUser {
   id: string;
   firstName: string;
   lastName: string;
+}
+
+export class User implements IUser {
+  public id: string;
+  public firstName: string;
+  public lastName: string;
+    constructor(
+      id: string,
+      firstName: string,
+      lastName: string,
+      ) {
+        this.firstName = firstName;
+        this.id = id;
+        this.lastName = lastName;
+    }
 }
