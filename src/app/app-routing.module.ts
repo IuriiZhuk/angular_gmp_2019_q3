b92ab6e1f1e10f1259bc1cdc5afe97a0c3be9852
Iurii Zhuk <iurii_zhuk@epam.com>
2019-11-19 11:07:58 +0300

    agm-t5 add path to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoursesComponent } from './courses/pages/courses/courses.component';
+import { LoginPageComponent } from './core/auth/containers/login-page/login-page.component';
 
 
 const routes: Routes = [
   { path: '', component: CoursesComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: '**', component: CoursesComponent},
 ];
 
 @NgModule({
