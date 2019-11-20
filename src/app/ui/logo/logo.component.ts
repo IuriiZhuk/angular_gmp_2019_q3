df3aa31650f3aa7fc7efe188c748da8e4309ae65
Iurii Zhuk <iurii_zhuk@epam.com>
2019-11-20 10:06:16 +0300

    agm-t5 add logo

--- src/app/ui/logo/logo.component.ts
+++ src/app/ui/logo/logo.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss']
 })
-export class LogoComponent implements OnInit {
+export class LogoComponent{
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
-  ngOnInit() {
-  }
-
+  public onClickHandler() {
+      this.router.navigate(['']);
+    }
 }
