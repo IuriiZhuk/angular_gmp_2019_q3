73f257182a769219ba29821ffaf5e53d864cebb3
Iurii Zhuk <iurii_zhuk@epam.com>
2019-12-26 10:04:58 +0300

    agm-t8-be add user token in interceptor

--- src/app/core/auth/interceptors/auth-interceptor.ts
+++ src/app/core/auth/interceptors/auth-interceptor.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpInterceptor, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpRequest, HttpHandler, HttpInterceptor, HttpEvent} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthorizationService} from '../service/authorization.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  constructor(
+    private authService: AuthorizationService,
+  ) {
+  }
 
-  const authReq = req.clone({
-  headers: req.headers.set('Authorization', 'agm-token')
-  });
-  return next.handle(authReq);
+  public token = this.authService.getUserTokenFromLocalStorage();
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', this.token)
+    });
+    return next.handle(authReq);
   }
-  }
+}
